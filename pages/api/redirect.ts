aa90a1742c2bff231ecdb57a68d9b1664253a0ce
0xhatsume <0xhatsume@gmail.com>
2024-03-17 06:24:49 +0000

    :white_check_mark: Test redirect to play route

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { BASE_URL } from "@/utils";
+import { NextRequest, NextResponse } from "next/server";
 
 export default async function handler(
   req: NextApiRequest,
@@ -26,6 +27,13 @@ export default async function handler(
     if (buttonIndex === 1){
       const messageBytes = signedMessage?.trustedData?.messageBytes;
       res.redirect(302, `${BASE_URL}/verify/${messageBytes}`);
+    } else {
+      return new NextResponse(`<!DOCTYPE html><html><head>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://jade-doubtful-hippopotamus-568.mypinata.cloud/ipfs/QmYU1StXRTHJG9GsX83sB2K6VQ1gsjdnpbPgrzHViBShyx" />
+        <meta property="fc:frame:button:1" content="directionszzz" />
+        <meta property="fc:frame:post_url" content="${BASE_URL}/api/play" />
+        </head></html>`);
     }
   } else {
     res.status(405).end(); // Method Not Allowed
