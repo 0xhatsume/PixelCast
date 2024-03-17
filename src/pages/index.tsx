b53584f0524d8c0c4b360b74955c5b117e91f817
0xhatsume <0xhatsume@gmail.com>
2024-03-17 04:05:57 +0000

    :sparkles: Convert to FrameMetadata use

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,34 @@
 import { BASE_URL } from "@/utils";
 import Head from "next/head";
+import type { Metadata, ResolvingMetadata } from "next";
+import { FrameMetadata, getFrameMetadata } from "@coinbase/onchainkit";
+
+
+export async function generateMetadata(): Promise<Metadata> {
+  const frameMetadata = getFrameMetadata({
+    buttons: [`Connect Starknet Wallet`,`Guest`],
+    image: ``,
+    post_url: `${BASE_URL}/api/redirect`,
+  });
+
+  return {
+    title: "PixeLaw - pixel-based Autonomous World",
+    description: "PixeLAW is a pixel-based autonomous world. And we provide the way to build a fully on-chain game.",
+    openGraph: {
+      title: "PixeLaw - pixel-based Autonomous World",
+      description: "PixeLAW is a pixel-based autonomous world. And we provide the way to build a fully on-chain game.",
+      images: [`https://jade-doubtful-hippopotamus-568.mypinata.cloud/ipfs/QmYU1StXRTHJG9GsX83sB2K6VQ1gsjdnpbPgrzHViBShyx`],
+    },
+    other: {
+      ...frameMetadata,
+    },
+  };
+}
 
 export default function Home() {
   return (
     <>
-      <Head>
+      {/* <Head>
         <meta property="og:title" content="Frame" />
         <meta property="og:image" content={`https://jade-doubtful-hippopotamus-568.mypinata.cloud/ipfs/QmYU1StXRTHJG9GsX83sB2K6VQ1gsjdnpbPgrzHViBShyx`} />
         <meta property="fc:frame" content="vNext" />
@@ -15,8 +39,8 @@ export default function Home() {
           property="fc:frame:post_url"
           content={`${BASE_URL}/api/redirect`}
         />
-      </Head>
-      Henlos Bro wif Local Storage v3 mapping PostGres
+      </Head> */}
+      Henlos Bro new FrameMetadata
     </>
   );
 }
