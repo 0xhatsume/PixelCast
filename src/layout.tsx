40634cfb5f25617551492c86ee3653ca92385b8c
0xhatsume <0xhatsume@gmail.com>
2024-03-17 04:44:36 +0000

    :bug: Shift metadata chunk to page index

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,35 +1,35 @@
-import { BASE_URL } from "@/utils";
-import type { Metadata, ResolvingMetadata } from "next";
-import { FrameMetadata, getFrameMetadata } from "@coinbase/onchainkit";
+// import { BASE_URL } from "@/utils";
+// import type { Metadata, ResolvingMetadata } from "next";
+// import { FrameMetadata, getFrameMetadata } from "@coinbase/onchainkit";
 
 
-export async function generateMetadata(): Promise<Metadata> {
-    const frameMetadata = getFrameMetadata({
-        buttons: [
-        {
-            label: "Connect Starknet Wallet",
-            action: "post_redirect",
-        }, 
-        {
-            label:"Play as Guest"
-        }],
-        image: ``,
-        post_url: `${BASE_URL}/api/redirect`,
-    });
+// export async function generateMetadata(): Promise<Metadata> {
+//     const frameMetadata = getFrameMetadata({
+//         buttons: [
+//         {
+//             label: "Connect Starknet Wallet",
+//             action: "post_redirect",
+//         }, 
+//         {
+//             label:"Play as Guest"
+//         }],
+//         image: ``,
+//         post_url: `${BASE_URL}/api/redirect`,
+//     });
 
-    return {
-        title: "PixeLaw - pixel-based Autonomous World",
-        description: "PixeLAW is a pixel-based autonomous world. And we provide the way to build a fully on-chain game.",
-        openGraph: {
-        title: "PixeLaw - pixel-based Autonomous World",
-        description: "PixeLAW is a pixel-based autonomous world. And we provide the way to build a fully on-chain game.",
-        images: [`https://jade-doubtful-hippopotamus-568.mypinata.cloud/ipfs/QmYU1StXRTHJG9GsX83sB2K6VQ1gsjdnpbPgrzHViBShyx`],
-        },
-        other: {
-        ...frameMetadata,
-        },
-    };
-}
+//     return {
+//         title: "PixeLaw - pixel-based Autonomous World",
+//         description: "PixeLAW is a pixel-based autonomous world. And we provide the way to build a fully on-chain game.",
+//         openGraph: {
+//         title: "PixeLaw - pixel-based Autonomous World",
+//         description: "PixeLAW is a pixel-based autonomous world. And we provide the way to build a fully on-chain game.",
+//         images: [`https://jade-doubtful-hippopotamus-568.mypinata.cloud/ipfs/QmYU1StXRTHJG9GsX83sB2K6VQ1gsjdnpbPgrzHViBShyx`],
+//         },
+//         other: {
+//         ...frameMetadata,
+//         },
+//     };
+// }
 
 export const viewport = {
 	width: "device-width",
