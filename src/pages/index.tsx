3922622f76a9212bdd7d0643069a54c8b694ff6f
0xhatsume <0xhatsume@gmail.com>
2024-03-17 03:22:55 +0000

    :bento: Change frame image to pixelaw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <>
       <Head>
         <meta property="og:title" content="Frame" />
-        <meta property="og:image" content={`${BASE_URL}/starknet.png`} />
+        <meta property="og:image" content={`https://jade-doubtful-hippopotamus-568.mypinata.cloud/ipfs/QmYU1StXRTHJG9GsX83sB2K6VQ1gsjdnpbPgrzHViBShyx`} />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content={`${BASE_URL}/starknet.png`} />
+        <meta property="fc:frame:image" content={`https://jade-doubtful-hippopotamus-568.mypinata.cloud/ipfs/QmYU1StXRTHJG9GsX83sB2K6VQ1gsjdnpbPgrzHViBShyx`} />
         <meta property="fc:frame:button:1" content="Connect Starknet Wallet" />
         <meta property="fc:frame:button:1:action" content="post_redirect" />
         <meta
